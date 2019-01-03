e272ff9015ad48e76e8692e957604cdbebb0a8fd
Jiwon Choi <jiwon.choi@kaist.ac.kr>
2019-01-03 18:59:43 +0900

    Add features for blacklisting

--- MnemonicHunter.py
+++ MnemonicHunter.py
@@ -3,66 +3,105 @@
 import requests
 import re
 import sys
-# https://c9x.me/x86/html/file_module_x86_id_15.html
-def find_nth(haystack, needle, n):
-    start = haystack.find(needle)
-    while start >= 0 and n > 1:
-        start = haystack.find(needle, start+len(needle))
-        n -= 1
-    return start
+from bs4 import BeautifulSoup
+
+def removebrackets(str):
+	while True:
+		if '<' in str and '>' in str:
+			idx1 = str.index('<')
+			idx2 = str.index('>')
+			str = str.replace(str[idx1:idx2+1],'')
+		else:
+			break
+	return str
 
 
+def html2dictionary(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    
+    table      = soup.find_all('table',{'class':'box'}) # <table class="box"></table>
+    tablelines = table[0].find_all('tr')
 
-def get_html(url):
-   _html = ""
-   resp = requests.get(url)
-   if resp.status_code == 200:
-      _html = resp.text
-      i1 = find_nth(_html,'<h1>',2)
-      i2 = find_nth(_html,'</h1>',2)
-      name = _html[i1 + len('<h1>'):i2]
-   return _html, name
+    resdic = {}
+    colomnNames = []
 
+    for e in tablelines:
+    	ths = e.find_all('th')
+    	tds = e.find_all('td')
 
+    	for i in xrange(len(ths)):
+    		# 칼럼이름 리스트화
+    		colomnNames.append(removebrackets(str(ths[i])))
+    		# 딕셔너리 초기화
+    		resdic[colomnNames[i]] = []
+
+    	for i in xrange(len(tds)):
+    		# 컨텐츠 스트립
+    		colomnContent = removebrackets(str(tds[i]))
+    		# 딕셔너리 어펜드
+    		resdic[colomnNames[i]].append(colomnContent)
+    return resdic
+
+def parseargv(sysargv):
+	whitelist = []
+	blacklist = []
+	mode = 'default'
+	for str in sysargv:
+		if str == '--blacklist':
+			mode = '--blacklist'
+			continue
+		elif str == '--whitelist':
+			mode = '--whitelist'
+			continue
+
+		if mode == '--blacklist':
+			blacklist.append(str)
+		elif mode == '--whitelist':
+			whitelist.append(str)
+
+	return blacklist, whitelist
 
 if __name__=="__main__":
-	# case 1. simple string
-	if len(sys.argv) < 2:
-		print "usage : python MnemonicHunter.py [MNEM-TO-SEARCH_01] [MNEM-TO-SEARCH_02] ..."
+
+	if len(sys.argv) < 3:
+		print "Usage)"
+		print "    python MnemonicHunter.py [OptionName] [Str1] [Str2] [...]"
+		print ""
+		print "Options)"
+		print "    --blacklist : specifies the string to be excluded from searching"
+		print "    --whitelist : specifies the string to be included from searching"
+		print ""
+		print "Example)"
+		print "     python MnemonicHunter.py --blacklist 'r/m32' --whitelist 'IMM' "
+		print ""
 		sys.exit()
 	
-	search = []
-	result = []
-	for i in range(1, len(sys.argv)):
-		search.append(sys.argv[i])
+	blacklist, whitelist = parseargv(sys.argv)
+	print ''
+	print "blacklist : {}".format(blacklist)
+	print "whitelist : {}".format(whitelist)
+	print ''
 	
-	print "Searching for : " + str(search)
-	
-	exist = []
+
 	for i in range(1,333): 
 		url = 'https://c9x.me/x86/html/file_module_x86_id_{}.html'.format(i)
-		text, name = get_html(url)
-		for s in search:
-			if s not in text: continue # filtering... 
-		exist.append(i)
-		count = text.count('<code>')
-		line_Mnemonic = 'dummy initialize'
-		for j in xrange(count):
-			start = text.index('<code>') + len('<code>')
-			end   = text.index('</code>')
-			before = line_Mnemonic 
-
-			line_Opcode = before # html사응로 바로 이전의 <code>...</code> 이 Opcode자리임. 
-			line_Mnemonic = text[start:end]
-			text = text[end + len('</code>'):]
-			for s in search:
-				if s in line_Mnemonic:
-					# print " "+line_Opcode + " ---> " + line_Mnemonic
-					if line_Mnemonic not in result:
-						print line_Mnemonic
-						result.append(line_Mnemonic)
-		print ''
-			
-	print exist
+		resdic = html2dictionary(url)
+		select = 'yes' # 디폴트 셋팅
+		for content in resdic['Mnemonic']:
+			for b in blacklist:
+				if b in content:
+					select = 'no'
+			for w in whitelist:
+				if w not in content:
+					select = 'no'
+			if select == 'yes': # 살아남았다면
+				print content
 
 
+		for c in resdic['Opcode']:
+			'nothing to do'
+
+		for c in resdic['Description']:
+			'nothing to do'
+	
