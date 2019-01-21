9c0b508984c29628061596876bfc8f01476e4f40
Jiwon Choi <jiwon.choi@kaist.ac.kr>
2019-01-21 22:17:39 +0900

    added --startfrom option. It is useful because sometimes network goes hell..:D Now I can start from specific # of url.

--- MnemonicHunter.py
+++ MnemonicHunter.py
@@ -5,6 +5,9 @@ import re
 import sys
 from bs4 import BeautifulSoup
 
+def lowercase(line):
+    return line.lower()
+
 def removebrackets(str):
 	while True:
 		if '<' in str and '>' in str:
@@ -39,6 +42,7 @@ def html2dictionary(url):
     	for i in xrange(len(tds)):
     		# 컨텐츠 스트립
     		colomnContent = removebrackets(str(tds[i]))
+    		colomnContent = lowercase(colomnContent)
     		# 딕셔너리 어펜드
     		resdic[colomnNames[i]].append(colomnContent)
     return resdic
@@ -46,21 +50,27 @@ def html2dictionary(url):
 def parseargv(sysargv):
 	whitelist = []
 	blacklist = []
+	startfrom = 1 # 디폴트로다가는 url 1부터 시작한다. 
 	mode = 'default'
+
 	for str in sysargv:
-		if str == '--blacklist':
+		if str == '--blacklist': # legacy... no more use
 			mode = '--blacklist'
 			continue
 		elif str == '--whitelist':
 			mode = '--whitelist'
 			continue
-
+		elif str == '--startfrom':
+			mode = '--startfrom'
+			continue
 		if mode == '--blacklist':
 			blacklist.append(str)
 		elif mode == '--whitelist':
 			whitelist.append(str)
+		elif mode == '--startfrom':
+			startfrom = int(str)
 
-	return blacklist, whitelist
+	return blacklist, whitelist, startfrom
 
 if __name__=="__main__":
 
@@ -69,36 +79,44 @@ if __name__=="__main__":
 		print "    python MnemonicHunter.py [OptionName] [Str1] [Str2] [...]"
 		print ""
 		print "Options)"
-		print "    --blacklist : specifies the string to be excluded from searching"
 		print "    --whitelist : specifies the string to be included from searching"
 		print ""
 		print "Example)"
-		print "     python MnemonicHunter.py --blacklist 'r/m32' --whitelist 'IMM' "
+		print "     python MnemonicHunter.py --whitelist 'IMM' "
 		print ""
 		sys.exit()
 	
-	blacklist, whitelist = parseargv(sys.argv)
+	blacklist, whitelist, startfrom = parseargv(sys.argv)
 	print ''
 	print "blacklist : {}".format(blacklist)
 	print "whitelist : {}".format(whitelist)
 	print ''
 	
 	#blacklist 부터 필터링하고나서, whitelist 매치되는넘들을 추가함.
-	for i in range(1,333): 
+	for i in range(startfrom,333): 
+		print i
 		url = 'https://c9x.me/x86/html/file_module_x86_id_{}.html'.format(i)
 		resdic = html2dictionary(url)
 		
 		for i in xrange(len(resdic['Mnemonic'])):
-			select  = 'yes' # 디폴트 셋팅
-			for b in blacklist:
-				if b in resdic['Mnemonic'][i]:
-					select = 'no'
+			select  = 'no' # 디폴트 셋팅
+			content = resdic['Mnemonic'][i]
+			content = content.replace(',',' ')
+			content = content.replace('(',' ')
+			content = content.replace(')',' ')
+			content = content.replace(':',' ')
+			content = content.replace('&',' ')
+			content = content.replace('*',' ')
+			content = content.split(' ')
 			for w in whitelist:
-				if w not in resdic['Mnemonic'][i]:
-					select = 'no'
+				for c in content: # 'ADC', 'r/m32', 'r16' 에 대해서 모두 검사
+					if w == c:
+						select = 'yes'
+			
 			if select == 'yes': # 살아남았다면
-				print resdic['Mnemonic'][i]
+				print resdic['Mnemonic'][i].ljust(70) + ' : ' + resdic['Opcode'][i]
 
+		# TODO: 잘되면 커밋 및 푸쉬. 고고. 푸쉬하면 이거 지우쟈아~
 
 		for i in xrange(len(resdic['Opcode'])):
 			'nothing to do'
