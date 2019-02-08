0d8aad3523b8871b89b08607912a43e11ff1d42c
Jiwon Choi <jiwon.choi@kaist.ac.kr>
2019-02-08 22:54:01 +0900

    Added --colomnname option

--- MnemonicHunter.py
+++ MnemonicHunter.py
@@ -5,6 +5,7 @@ import re
 import sys
 from bs4 import BeautifulSoup
 
+
 def lowercase(line):
     return line.lower()
 
@@ -27,7 +28,7 @@ def html2dictionary(url):
     tablelines = table[0].find_all('tr')
 
     resdic = {}
-    colomnNames = []
+    colomnnames = []
 
     for e in tablelines:
     	ths = e.find_all('th')
@@ -35,68 +36,80 @@ def html2dictionary(url):
 
     	for i in xrange(len(ths)):
     		# 칼럼이름 리스트화
-    		colomnNames.append(removebrackets(str(ths[i])))
+    		colomnnames.append(removebrackets(str(ths[i])))
     		# 딕셔너리 초기화
-    		resdic[colomnNames[i]] = []
+    		resdic[colomnnames[i]] = []
 
     	for i in xrange(len(tds)):
     		# 컨텐츠 스트립
     		colomnContent = removebrackets(str(tds[i]))
     		colomnContent = lowercase(colomnContent)
     		# 딕셔너리 어펜드
-    		resdic[colomnNames[i]].append(colomnContent)
+    		resdic[colomnnames[i]].append(colomnContent)
     return resdic
 
 def parseargv(sysargv):
-	whitelist = []
-	startfrom = 1 # 디폴트로다가는 url 1부터 시작한다. 
-	mode = 'default'
+	search = [] # TODO: 이거 이름을 화이트리스트가 아니라 search 로 바꺼야딩 ㅎ 
+	startfrom = 1  # 디폴트로다가는 url 1부터 시작한다. 
+	colomnname = '' 
 
+	mode = ''
 	for str in sysargv:
-		if str == '--whitelist':
-			mode = '--whitelist'
+		if str == '--search':
+			mode = '--search'
 			continue
 		elif str == '--startfrom':
 			mode = '--startfrom'
 			continue
-		
-		if mode == '--whitelist':
-			whitelist.append(str)
+		elif str == '--colomnname':
+			mode = '--colomnname'
+			continue
+
+		if mode == '--search':
+			search.append(str)
 		elif mode == '--startfrom':
 			startfrom = int(str)
+		elif mode == '--colomnname':
+			colomnname = str
 
-	return  whitelist, startfrom
+	return  search, startfrom, colomnname
+
+def exitproc():
+	print "Usage)"
+	print "    python MnemonicHunter.py [OptionName] [Str1] [Str2] [...]"
+	print ""
+	print "Options)"
+	print "    --search  : specifies the string to be included from searching"
+	print "    --colomnname : specifies the colomn name. Mnemonic/Opcode/Description"
+	print "    --startfrom  : specifies the page number of c9x.me/x86 to start searching (Optional)"
+	print ""
+	print "Example)"
+	print "     python MnemonicHunter.py --search 'imm316' 'imm32' --colomnname 'Mnemonic' --startfrom '292'"
+	print ""
+	sys.exit()
+
 
 if __name__=="__main__":
 
 	if len(sys.argv) < 3:
-		print "Usage)"
-		print "    python MnemonicHunter.py [OptionName] [Str1] [Str2] [...]"
-		print ""
-		print "Options)"
-		print "    --whitelist : specifies the string to be included from searching"
-		print "    --startfrom : specifies the page number of c9x.me/x86 to start searching"
+		exitproc()
 
-		print ""
-		print "Example)"
-		print "     python MnemonicHunter.py --whitelist 'imm316' 'imm32'"
-		print "     python MnemonicHunter.py --whitelist 'r/m16' --startfrom '292' "
-		print ""
-		sys.exit()
+	search, startfrom, colomnname = parseargv(sys.argv)
 	
-	whitelist, startfrom = parseargv(sys.argv)
-	print ''
-	print "whitelist : {}".format(whitelist)
-	print ''
-	
+	if len(search) is 0:
+		exitproc() 
+	if colomnname == '':
+		exitproc()
+	if colomnname not in ['Mnemonic', 'Opcode', 'Description']:
+		exitproc()
+
 	for i in range(startfrom,333): 
-		# print i
 		url = 'https://c9x.me/x86/html/file_module_x86_id_{}.html'.format(i)
 		resdic = html2dictionary(url)
 		
-		for i in xrange(len(resdic['Mnemonic'])):
+		for i in xrange(len(resdic[colomnname])):
 			select  = 'no' # 디폴트 셋팅
-			content = resdic['Mnemonic'][i]
+			content = resdic[colomnname][i]
 			content = content.replace(',',' ')
 			content = content.replace('(',' ')
 			content = content.replace(')',' ')
@@ -104,17 +117,12 @@ if __name__=="__main__":
 			content = content.replace('&',' ')
 			content = content.replace('*',' ')
 			content = content.split(' ')
-			for w in whitelist:
+			for w in search:
 				for c in content: # 'ADC', 'r/m32', 'r16' 에 대해서 모두 검사
 					if w == c:
 						select = 'yes'
 			
 			if select == 'yes': # 살아남았다면
-				print resdic['Mnemonic'][i].ljust(70) + ' : ' + resdic['Opcode'][i]
+				print resdic[colomnname][i].ljust(70) + ' : ' + resdic['Opcode'][i]
 
-		for i in xrange(len(resdic['Opcode'])):
-			'nothing to do'
 
-		for i in xrange(len(resdic['Description'])):
-			'nothing to do'
-	
